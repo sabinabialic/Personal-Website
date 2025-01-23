6c41af6903ad8c6efe71a58619f92253830adaed
sabinabialic <sabina.bialic@gmail.com>
2025-01-23 08:55:35 -0500

    Updated Jan 2025

--- src/components/layouts/Experience.js
+++ src/components/layouts/Experience.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Experience.css';
+import hashicorpLogo from '../../assets/images/hashi.png';
 import scoreLogo from '../../assets/images/penn.png';
 import sapLogo from '../../assets/images/sap.png';
 import esdcLogo from '../../assets/images/esdc.png';
@@ -19,33 +20,46 @@ export default class Experience extends Component {
               </div>
 
               <div class="parent">
-              <div class="div1">
-                    <div className="card company-thescore">
-                      <img src={scoreLogo} alt="thescore" className="company-image-thescore" />
-                      <div className="overlay-thescore">
-                        <div className="company-desc text-black">
-                          <h4>PENN Interactive | theScore</h4>
-                          <h5>Release Engineer</h5>
-                          <p>April 2024 - Present</p>
-                        </div>
+                <div class="div1">
+                  <div className="card company-hashicorp">
+                    <img src={hashicorpLogo} alt="hashicorp" className="company-image-hashicorp" />
+                    <div className="overlay-hashicorp">
+                      <div className="company-desc text-black">
+                        <h4>HashiCorp</h4>
+                        <h5>Software Engineer, Developer Workflows</h5>
+                        <p>February 2025 - Present</p>
                       </div>
                     </div>
                   </div>
+                </div>
 
                 <div class="div2">
-                    <div className="card company-sap">
-                      <img src={sapLogo} alt="SAP" className="company-image-sap" />
-                      <div className="overlay-sap">
-                        <div className="company-desc text-black">
-                          <h4>SAP</h4>
-                          <h5>DevOps Developer</h5>
-                          <p>July 2022 - April 2024</p>
-                        </div>
+                  <div className="card company-thescore">
+                    <img src={scoreLogo} alt="thescore" className="company-image-thescore" />
+                    <div className="overlay-thescore">
+                      <div className="company-desc text-black">
+                        <h4>PENN Entertainment | theScore</h4>
+                        <h5>Release Engineer, SRE</h5>
+                        <p>April 2024 - January 2025</p>
                       </div>
                     </div>
                   </div>
+                </div>
 
                 <div class="div3">
+                  <div className="card company-sap">
+                    <img src={sapLogo} alt="SAP" className="company-image-sap" />
+                    <div className="overlay-sap">
+                      <div className="company-desc text-black">
+                        <h4>SAP</h4>
+                        <h5>DevOps Developer</h5>
+                        <p>July 2022 - April 2024</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="div4">
                   <div className="card company-esdc">
                     <img src={esdcLogo} alt="ESDC" className="company-image-esdc" />
                     <div className="overlay-esdc">
@@ -55,19 +69,6 @@ export default class Experience extends Component {
                         <p>May 2021 - July 2022</p>
                         <h5>Developer Co-op</h5>
                         <p>January 2020 - May 2021</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="div4">
-                  <div className="card company-virtamove">
-                    <img src={virtamoveLogo} alt="VirtaMove" className="company-image-virtamove" />
-                    <div className="overlay-virtamove">
-                      <div className="company-desc text-black">
-                        <h4>VirtaMove</h4>
-                        <h5>Software Developer Co-op</h5>
-                        <p>May 2019 - August 2019</p>
                       </div>
                     </div>
                   </div>
